3733ece89e55ca0f87d43dc235a6f66d8f2c1c4f
Luca Marfia <102804579+morephire@users.noreply.github.com>
2023-01-09 19:36:37 +0100

    Add part 5 -ex 2 :white_check_mark:

---
+++ part05-02_number_of_elements/src/number_of_elements.py
@@ -0,0 +1,14 @@
+def count_matching_elements(my_matrix: list, element: int):
+    count = 0
+
+    for i in range(len(my_matrix)):
+        for j in range(len(my_matrix[i])):
+            if my_matrix[i][j] == element:
+                count +=1 
+    
+    return count
+
+if __name__ == "__main__":
+    m = [[1, 2, 3], [2, 3, 1], [4, 5, 6]]
+    print(count_matching_elements(m, 1))
+
